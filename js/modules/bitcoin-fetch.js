959f433fcee055cbddba5cf6958e0b8030ab07fd
Danilo <danmil-20@hotmail.com>
2022-10-06 16:12:35 -0300

    refatorado fetch bitcoin

--- js/modules/bitcoin-fetch.js
+++ js/modules/bitcoin-fetch.js
@@ -1,10 +1,10 @@
-export default function initBitcoinFetch() {
+export default function bitcoinFetch(url, target) {
   async function fetchBtc() {
     try {
-      const btcFetch = await fetch("https://blockchain.info/ticker");
+      const btcFetch = await fetch(url);
       const btcJSON = await btcFetch.json();
       const btc100 = 100 / btcJSON.BRL.sell;
-      const divBtc = document.querySelector(".btc-preco");
+      const divBtc = document.querySelector(target);
       divBtc.innerText = +btc100.toFixed(4);
     } catch (erro) {
       console.log(Error(erro));
